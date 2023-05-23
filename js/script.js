284f455864052a87ff8f895812a375315a656614
Teodor Agape <teodor.agape@yahoo.com>
2023-05-23 18:12:49 +0200

    html elements created manipulating DOM

--- js/script.js
+++ js/script.js
@@ -1,10 +1,63 @@
 "use strict";
 
-// Selecting elements
-const counterValue = document.querySelector(".counter-value");
-const addBtn = document.querySelector(".add");
-const subtractBtn = document.querySelector(".subtract");
-const resetBtn = document.querySelector(".reset");
+// Selecting the main element
+const main = document.querySelector(".main");
+
+// Creating the container element
+
+const container = document.createElement("div");
+container.classList.add("container");
+main.appendChild(container);
+
+// Creating the upper-side div
+const upperSide = document.createElement("div");
+upperSide.classList.add("upper-side");
+container.appendChild(upperSide);
+
+// Creating the counter value element
+
+const counterValue = document.createElement("p");
+counterValue.classList.add("counter-value");
+counterValue.textContent = 0;
+upperSide.appendChild(counterValue);
+
+// Creating the lower side div
+
+const lowerSide = document.createElement("div");
+lowerSide.classList.add("lower-side");
+container.appendChild(lowerSide);
+
+// Creating the buttons div
+
+const buttons = document.createElement("div");
+buttons.classList.add("buttons");
+lowerSide.appendChild(buttons);
+
+// Creating the add button
+
+const addBtn = document.createElement("button");
+addBtn.classList.add("add");
+addBtn.textContent = "+";
+buttons.appendChild(addBtn);
+
+// Creating the subtract button
+
+const subtractBtn = document.createElement("button");
+subtractBtn.classList.add("subtract");
+subtractBtn.textContent = "-";
+buttons.appendChild(subtractBtn);
+
+// Creating the reset div
+const resetContainer = document.createElement("div");
+resetContainer.classList.add("reset-container");
+lowerSide.appendChild(resetContainer);
+
+// Creating the reset button
+
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("reset");
+resetBtn.textContent = "Reset";
+resetContainer.appendChild(resetBtn);
 
 let total;
 
