b9831a7b531252f232d1da46e7e703c474b9c2ef
Teodor Agape <teodor.agape@yahoo.com>
2023-06-22 22:09:35 +0200

    refactored js code

--- js/script.js
+++ js/script.js
@@ -3,61 +3,29 @@
 // Selecting the main element
 const main = document.querySelector(".main");
 
-// Creating the container element
+// function that creates elements
+function createElement(name, type, className) {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  name.appendChild(element);
+  return element;
+}
 
-const container = document.createElement("div");
-container.classList.add("container");
-main.appendChild(container);
+// creating the DOM elements
+const container = createElement(main, "div", "container");
+const upperSide = createElement(container, "div", "upper-side");
+const counterValue = createElement(upperSide, "p", "counter-value");
+const lowerSide = createElement(container, "div", "lower-side");
+const buttons = createElement(lowerSide, "div", "buttons");
+const addBtn = createElement(buttons, "button", "add");
+const subtractBtn = createElement(buttons, "button", "subtract");
+const resetContainer = createElement(lowerSide, "div", "reset-container");
+const resetBtn = createElement(resetContainer, "button", "reset");
 
-// Creating the upper-side div
-const upperSide = document.createElement("div");
-upperSide.classList.add("upper-side");
-container.appendChild(upperSide);
-
-// Creating the counter value element
-
-const counterValue = document.createElement("p");
-counterValue.classList.add("counter-value");
-counterValue.textContent = 0;
-upperSide.appendChild(counterValue);
-
-// Creating the lower side div
-
-const lowerSide = document.createElement("div");
-lowerSide.classList.add("lower-side");
-container.appendChild(lowerSide);
-
-// Creating the buttons div
-
-const buttons = document.createElement("div");
-buttons.classList.add("buttons");
-lowerSide.appendChild(buttons);
-
-// Creating the add button
-
-const addBtn = document.createElement("button");
-addBtn.classList.add("add");
+// setting the textcontent of the elements
 addBtn.textContent = "+";
-buttons.appendChild(addBtn);
-
-// Creating the subtract button
-
-const subtractBtn = document.createElement("button");
-subtractBtn.classList.add("subtract");
 subtractBtn.textContent = "-";
-buttons.appendChild(subtractBtn);
-
-// Creating the reset div
-const resetContainer = document.createElement("div");
-resetContainer.classList.add("reset-container");
-lowerSide.appendChild(resetContainer);
-
-// Creating the reset button
-
-const resetBtn = document.createElement("button");
-resetBtn.classList.add("reset");
 resetBtn.textContent = "Reset";
-resetContainer.appendChild(resetBtn);
 
 let total;
 
